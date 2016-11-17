d990813c1a7a3703ba8ba6c4efdb58c7e97cde82
tomeehan <tom.meehan@me.com>
2016-11-17 23:15:53 +0000

    added 'mark as active' method on submit button + tick controller tempearture input conditional

--- app/controllers/ticks_controller.rb
+++ app/controllers/ticks_controller.rb
@@ -106,5 +106,11 @@ class TicksController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def tick_params
       params.require(:tick).permit(:complete, :temperature, :task_id, :category_id, :staff_id, :active)
+
+        if params[:active]
+          return params.require(:tick).permit(:name, :body).merge(active: true)
+        else    
+          return params.require(:tick).permit(:name, :body).merge(active: false)
+        end
     end
 end
