39c91bc549f42c8b3fc92fd0acdba45d639c95c2
tomeehan <tom.meehan@me.com>
2017-01-09 19:52:39 +0000

    added makers MVC for experience design program

---
+++ app/views/makers/_maker.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! maker, :id, :first_name, :last_name, :email, :created_at, :updated_at
+json.url maker_url(maker, format: :json)
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170109152409) do
+ActiveRecord::Schema.define(version: 20170109170404) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,14 @@ ActiveRecord::Schema.define(version: 20170109152409) do
   end
 
   create_table "companies", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "makers", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
