86601ad68e162b1df3273e4f59802dd08e2b8d5a
tomeehan <tom.meehan@me.com>
2017-01-18 18:14:12 +0000

    uncommented valiations on task'

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,15 +4,15 @@ class Task < ApplicationRecord
 	has_many :ticks
 	belongs_to :regularity
 
-	# validates :name, presence: true
-	# validates :regularity_id, presence: true
-	# validates :category_id, presence: true
+	validates :name, presence: true
+	validates :regularity_id, presence: true
+	validates :category_id, presence: true
 
-	# validate :has_content
+	validate :has_content
 
- #  	def has_content
- #  		if starting == false && midday == false && ending == false
- #    		errors[:base] = "You must select one"
- #  		end
-	# end
+  	def has_content
+  		if starting == false && midday == false && ending == false
+    		errors[:base] = "You must select one"
+  		end
+	end
 end
