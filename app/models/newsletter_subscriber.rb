b261e901f8b6ec422a5750ccfc20ff054907f10b
tomeehan <tom.meehan@me.com>
2017-01-18 06:36:22 +0000

    added client side validations to newsleetter_subscribers#new

--- app/models/newsletter_subscriber.rb
+++ app/models/newsletter_subscriber.rb
@@ -1,2 +1,8 @@
 class NewsletterSubscriber < ApplicationRecord
+
+	validates_presence_of :first_name, message: 'Please enter a first name'
+	validates_presence_of :email, message: 'Please enter a email address'
+
+
+	validates_format_of :email,:with => Devise::email_regexp, message: '...this doesn\'t look like an email address'
 end
