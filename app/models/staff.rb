5363c12b6f72a4c8560c22467cece42a99f4161f
tomeehan <tom.meehan@me.com>
2017-01-18 06:26:19 +0000

    client_side_validations gem; client_side_validations-simple-form gem; added client side validations to User#new, Staff#New, Tasks#new

--- app/models/staff.rb
+++ app/models/staff.rb
@@ -2,5 +2,10 @@ class Staff < ApplicationRecord
 	has_many :ticks
 	belongs_to :user
 
-	validates_presence_of :first_name, :surname, :email, :job_title
+	validates_presence_of :first_name, message: 'Please enter a first name'
+	validates_presence_of :surname, message: 'Please enter a surname'
+	validates_presence_of :email, message: 'Please enter a email address'
+	validates_presence_of :job_title, message: 'Please enter a job title'
+
+	validates_format_of :email,:with => Devise::email_regexp, message: '...this doesn\'t look like an email addres'
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -13,5 +13,12 @@ class User < ApplicationRecord
   	devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+    validates_presence_of :first_name, message: "Please enter your first name."
+    validates_presence_of :last_name, message: "Please enter your surname."
+    validates_presence_of :email, message: "Please enter an email address"
+    validates_presence_of :organisation, message: "What's your place called?"
+    validates_presence_of :password, message: "Please enter a password"
+    validates_presence_of :password_confirmation, message: "Please confirm your password"
 
+    validates_format_of :email, :with => Devise::email_regexp, message: '...this doesn\'t look like an email addres'
 end
