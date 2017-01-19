be7eba05f9dc1b99c8700e8d70cf4470c2c7b5e2
tomeehan <tom.meehan@me.com>
2017-01-19 16:17:49 +0000

    commented organsations validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,7 @@ class User < ApplicationRecord
     validates_presence_of :first_name, message: "Please enter your first name."
     validates_presence_of :last_name, message: "Please enter your surname."
     validates_presence_of :email, message: "Please enter an email address"
-    validates_presence_of :organisation, message: "What's your place called?"
+    # validates_presence_of :organisation, message: "What's your place called?"
     
 
     validates_format_of :email, :with => Devise::email_regexp, message: '...this doesn\'t look like an email address'
