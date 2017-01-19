2b59d69cebd9814689a885225e7b9a6c13d48bc3
tomeehan <tom.meehan@me.com>
2017-01-19 01:51:25 +0000

    removed more validations from user model

--- app/controllers/subscribers_controller.rb
+++ app/controllers/subscribers_controller.rb
@@ -16,7 +16,7 @@ class SubscribersController < ApplicationController
 
 		current_user.subscribed = true 
 		current_user.stripe_id = customer.id
-		current_user.save
+		current_user.save!
 
 		redirect_to staffs_path
 	end
