5f86d0b62da9597d70d3b48648c7ca11274fc381
tomeehan <tom.meehan@me.com>
2017-01-23 22:00:28 +0000

    set #features to root

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
 
   # match 'ticks/completed' => 'ticks#completed', :via => [:get],:as => 'completed_ticks'
 
-  root 'pages#home'
+  root 'pages#features'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get "/home", :to => 'pages#home'
