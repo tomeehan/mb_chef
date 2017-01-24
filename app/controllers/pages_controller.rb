f13681837d7517b7fcefa44b181d42dede5abef0
tomeehan <tom.meehan@me.com>
2017-01-24 15:01:15 +0000

    removed redirect_to from home action

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,6 @@
 class PagesController < ApplicationController
 
 	def home
-		redirect_to pricing
-
 		if user_signed_in?
 			redirect_to tasks_path
 			# TODO replace this with https://github.com/plataformatec/devise/wiki/How-To:-redirect-to-a-specific-page-on-successful-sign-in
