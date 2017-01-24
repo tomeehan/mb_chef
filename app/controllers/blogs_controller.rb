b8df9c05b96b64b4c5a91efd4e79f30dc4b2a664
tomeehan <tom.meehan@me.com>
2017-01-24 19:12:18 +0000

    added reading_time to blog index, show, _form

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -71,6 +71,6 @@ class BlogsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def blog_params
-      params.require(:blog).permit(:title, :content, :author, :meta_desc, :lead, :image_link)
+      params.require(:blog).permit(:title, :content, :author, :meta_desc, :lead, :image_link, :reading_time)
     end
 end
