3a15c50f165aaf315f095133e13435585ee52ff6
tomeehan <tom.meehan@me.com>
2017-01-24 14:59:23 +0000

    m

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,6 @@ Rails.application.routes.draw do
 
   root 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
   get "/home", :to => 'pages#home'
   get "/pricing", :to => 'pages#pricing'
   get "/about", :to => 'pages#about'
